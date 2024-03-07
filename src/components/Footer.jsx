73591196f0b38bca0fb6006c17c4758cafd61386
Hvbert1 <henryjiafu@gmail.com>
2024-03-07 18:53:16 +1100

    fix footer + header styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,28 @@
 export default function Footer() {
   return (
     <>
-      <div className="footer">
-        <div className="footerCard">
-          <h2>Location</h2>
-          <p>26 Carmel Pl, Winston Hills</p>
-        </div>
-        <div className="footerCard">
-          <h2>Find us online</h2>
-          <div className="icons">
-            <i className="devicon-facebook-plain"></i>
-            <i className="devicon-linkedin-plain"></i>
-            <i className="devicon-twitter-original"></i>
+      <div className="footerContainer">
+        <div className="footerContent">
+          <div className="footerCard">
+            <h2>Location</h2>
+            <p>26 Carmel Pl, Winston Hills</p>
           </div>
-        </div>
-        <div className="footerCard">
-          <h2>About SR Tutoring</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Elementum nisi quis eleifend quam adipiscing vitae.
-          </p>
+          <div className="footerCard">
+            <h2>Find us online</h2>
+            <div className="icons">
+              <i className="devicon-facebook-plain"></i>
+              <i className="devicon-linkedin-plain"></i>
+              <i className="devicon-twitter-original"></i>
+            </div>
+          </div>
+          <div className="footerCard">
+            <h2>About SR Tutoring</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Elementum nisi quis eleifend quam adipiscing vitae.
+            </p>
+          </div>
         </div>
       </div>
     </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,24 +3,26 @@ import tutorLogo from "../assets/logo.png";
 export default function Navbar() {
   return (
     <>
-      <nav className="nav">
-        <a href="/home" target="_blank">
-          <img src={tutorLogo} className="logo" alt="Tutoring logo" />
-        </a>
-        <ul>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/classes">Classes</a>
-          </li>
-          <li>
-            <a href="/contact">Contact Us</a>
-          </li>
-          <li>
-            <a href="/login">Login</a>
-          </li>
-        </ul>
+      <nav className="navContainer">
+        <div className="navContent">
+          <a href="/home" target="_blank">
+            <img src={tutorLogo} className="logo" alt="Tutoring logo" />
+          </a>
+          <ul>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/classes">Classes</a>
+            </li>
+            <li>
+              <a href="/contact">Contact Us</a>
+            </li>
+            <li>
+              <a href="/login">Login</a>
+            </li>
+          </ul>
+        </div>
       </nav>
     </>
   );
