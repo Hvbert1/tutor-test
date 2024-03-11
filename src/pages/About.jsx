59d1a87043374e4b2750adbba6cb05ebadc0e043
Hvbert1 <henryjiafu@gmail.com>
2024-03-11 12:35:26 +1100

    extracted tutor data separately

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,9 @@
 import mainPic from "../assets/homePic.jpg";
-import tutor1 from "../assets/tutors/1.png";
-import tutor2 from "../assets/tutors/2.png";
-import tutor3 from "../assets/tutors/3.png";
-import tutor4 from "../assets/tutors/4.png";
-import tutor5 from "../assets/tutors/5.png";
-import tutor6 from "../assets/tutors/6.png";
-import tutor7 from "../assets/tutors/7.png";
 import "./About.css";
 import React, { useRef, useState } from "react";
 import TutorList from "../components/TutorList";
 import Modal from "../components/Modal";
+import tutors from "../components/tutorData";
 
 export default function About() {
   const [dialogContent, setDialogContent] = useState(null);
@@ -23,51 +17,6 @@ export default function About() {
       ? dialogRef.current.close()
       : dialogRef.current.showModal();
   };
-
-  const tutors = [
-    {
-      id: 1,
-      name: "Sanchea Rodrigues",
-      pictureUrl: tutor1,
-      description: "Founder + Primary School Teacher",
-    },
-    {
-      id: 2,
-      name: "Ananya",
-      pictureUrl: tutor2,
-      description: "Math and English tutor",
-    },
-    {
-      id: 3,
-      name: "Andrea",
-      pictureUrl: tutor3,
-      description: "Math and English tutor",
-    },
-    {
-      id: 4,
-      name: "Sai",
-      pictureUrl: tutor4,
-      description: "Math and English tutor",
-    },
-    {
-      id: 5,
-      name: "Venkat",
-      pictureUrl: tutor5,
-      description: "Math and English tutor",
-    },
-    {
-      id: 6,
-      name: "Vy",
-      pictureUrl: tutor6,
-      description: "Math, English and Piano tutor",
-    },
-    {
-      id: 7,
-      name: "Yen",
-      pictureUrl: tutor7,
-      description: "Math and English tutor",
-    },
-  ];
 
   return (
     <>
