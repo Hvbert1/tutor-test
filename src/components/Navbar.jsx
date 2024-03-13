19e0170bf01f6980bd8e958585f78ac9eadc1a36
Hvbert1 <henryjiafu@gmail.com>
2024-03-13 13:49:46 +1100

    create dropdown for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,13 @@
+import React, { useState } from "react";
 import tutorLogo from "../assets/logo.png";
 
 export default function Navbar() {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
   return (
     <>
       <nav className="navContainer">
@@ -8,7 +15,7 @@ export default function Navbar() {
           <a href="/home">
             <img src={tutorLogo} className="logo" alt="Tutoring logo" />
           </a>
-          <ul>
+          <ul className="navHeader">
             <li>
               <a href="/about">About</a>
             </li>
@@ -22,8 +29,24 @@ export default function Navbar() {
               <a href="/login">Login</a>
             </li>
           </ul>
+          <li className="navDropdown">
+            <span
+              className="material-symbols-outlined"
+              onClick={toggleDropdown}
+            >
+              menu
+            </span>
+          </li>
         </div>
       </nav>
+      {dropdownOpen && (
+        <div className="dropdownContent">
+          <a href="/about">About</a>
+          <a href="/classes">Classes</a>
+          <a href="/contact">Contact Us</a>
+          <a href="/login">Login</a>
+        </div>
+      )}
     </>
   );
 }
